78e3c25c423200723e64d34690827b84bcbca260
NathanDavids <129671319+NathanDavids@users.noreply.github.com>
2024-03-07 13:20:08 +0200

    Added Register Components & Function

--- src/components/Authenticate.svelte
+++ src/components/Authenticate.svelte
@@ -3,6 +3,7 @@
     let password = "";
     let confirmPassword = "";
     let error = false;
+    let register = false;
 
     function handleAuthenticate(){
         if (!email || !password || (register && !confirmPassword)){
@@ -10,11 +11,16 @@
             return
         }
     }
+
+    function handleRegister(){
+        register = !register;
+    }
+    
 </script>
 
 <div class="authContainer">
     <form>
-        <h1>Login</h1>
+        <h1>{register ? "Register" : "Login"}</h1>
         {#if error}
             <p class="error"> The information you have entered is not correct.</p>
         {/if}
@@ -26,12 +32,28 @@
             <p class={password ? 'above' : 'center'}>Password</p>
             <input bind:value={password} type="password" placeholder="Password" />
         </label>
-        <label>
-            <p class={confirmPassword ? 'above' : 'center'}>Confirm Password</p>
-            <input bind:value={confirmPassword} type="password" placeholder="Confirm Password" />
-        </label>
+        {#if register}
+            <label>
+                <p class={confirmPassword ? 'above' : 'center'}>Confirm Password</p>
+                <input bind:value={confirmPassword} type="password" placeholder="Confirm Password" />
+            </label>
+        {/if}
         <button type="button">Submit</button>
     </form>
+    <div class="options">
+        <p>Or</p>
+        {#if register}
+            <div>
+                <p>Already have an account?</p>
+                <p on:click={handleRegister} on:keydown={() => {}}>Login</p>
+            </div>
+        {:else}
+            <div>
+                <p>Don't have an account?</p>
+                <p on:click={handleRegister} on:keydown={() => {}}>Register</p>
+            </div>  
+        {/if}
+    </div>
 </div>
 
 <style>
@@ -48,6 +70,10 @@
         display: flex;
         flex-direction: column;
         gap: 14px;
+    }
+
+    form,
+    .options{
         width: 400px;
         max-width: 100%;
         margin: 0 auto;
@@ -78,6 +104,10 @@
     form input:focus{
         border: none;
         outline: none;
+    }
+
+    form label:focus-within{
+        border-color: blue;
     }
 
     form button{
@@ -123,6 +153,53 @@
     .error{
         color: coral;
         font-size: 0.9rem;
+    }
+
+    .options{
+        padding: 14px 0;
+        overflow: hidden;
+        font-size: 0.9rem;
+        flex-direction: column;
+        gap: 4px;
+    }
+
+    .options > p {
+        position: relative;
+        text-align: center;
+        width: fit-content;
+        margin: 0 auto;
+        padding: 0 8px;
+    }
+
+    .options > p::after,
+    .options > p::before {
+        position: absolute;
+        content: '';
+        top: 50%;
+        transform: translateY(-50%);
+        width: 100vw;
+        height: 1.5px;
+        background: white;
+    }
+
+    .options > p::after {
+        right: 100%;
+    }
+
+    .options > p::before {
+        left: 100%;
+    }
+
+    .options div{
+        display: flex;
+        align-items: center;
+        gap: 8px;
+        justify-content: center;
+    }
+
+    .options div p:last-of-type{
+        color: cyan;
+        cursor: pointer;
     }
 
 </style>
